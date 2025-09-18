a46584a89240094eedba8b00177f0c3882b08739
Andrew Tippey <atippey@gmail.com>
2025-09-18 10:37:10 -0400

    Allowing empty ansible environment for testing

--- dnd/ansible_install.sh
+++ dnd/ansible_install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+
 virtualenv venv
 . venv/bin/activate
 pip install ansible
 cd /project
-ansible-playbook playbooks/site.yml -i dnd/inventory.yml
+
+if [[ -n $K3S_INIT ]]; then
+    ansible-playbook playbooks/site.yml -i dnd/inventory.yml
+else
+    echo Leaving container up for ansible control!
+    while true; do sleep 15; done
+fi
