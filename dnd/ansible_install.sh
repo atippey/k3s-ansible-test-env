72e9dc408309022384619f51ef3b5697392f0f96
Andrew Tippey <Andrew.Tippey@honeywell.com>
2025-09-18 15:23:32 -0400

    Fixed ansible init disable

--- dnd/ansible_install.sh
+++ dnd/ansible_install.sh
@@ -8,7 +8,7 @@ cd /project
 
 if [[ -n $K3S_INIT ]]; then
     ansible-playbook playbooks/site.yml -i dnd/inventory.yml
-else
-    echo Leaving container up for ansible control!
-    while true; do sleep 15; done
 fi
+
+echo Leaving container up for ansible control!
+while true; do sleep 15; done
